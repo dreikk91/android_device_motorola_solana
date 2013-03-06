a86b63d11f0db953b569208add562ccab91ddebc
dhacker29 <davidhackerdvm@gmail.com>
2013-03-06 02:59:59 -0600

    This is phablet only need cm parts

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,12 +1,1 @@
-ifneq ($(filter full_solana aokp_solana cm_solana killrom_solana cna_solana,$(TARGET_PRODUCT)),)
 PRODUCT_MAKEFILES := $(LOCAL_DIR)/full_solana.mk
-ifeq ($(TARGET_PRODUCT),aokp_solana)
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/aokp.mk
-endif
-ifeq ($(TARGET_PRODUCT),cna_solana)
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/cna.mk
-endif
-ifeq ($(TARGET_PRODUCT),killrom_solana)
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/killrom.mk
-endif
-endif
